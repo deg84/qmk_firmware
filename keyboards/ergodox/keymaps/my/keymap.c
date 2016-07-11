14f49c5b2af13998c33e8934ce36eaa6bf3e27d7
deg84 <t.deg84@gmail.com>
2016-07-12 08:46:38 +0900

    mac向けに調整

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -14,14 +14,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Esc  | Grv  |Insert| Left | Right|                                       |  Up  | Down |   [  |   ]  |  L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Ctrl | Alt  |       | LGui | Ctrl |
+ *                                        | LGui | Alt  |       | Alt  | RGui |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      | Home |       | PgUp |      |      |
  *                                 |Backsp| Del  |------|       |------| Enter| Space|
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_LEFT,
         KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  TG(SYMB),
-        KC_CAPS,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
         KC_ESC,   KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
-                                               KC_LCTL,KC_LALT,
+                                               KC_LGUI,KC_LALT,
                                                        KC_HOME,
                                          KC_BSPC,KC_DEL,KC_END,
         // right hand
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                   KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,  KC_FN2,
-             KC_LGUI, KC_RCTL,
+             KC_RALT, KC_RGUI,
              KC_PGUP,
              KC_PGDN, KC_ENT, KC_SPC
     ),
