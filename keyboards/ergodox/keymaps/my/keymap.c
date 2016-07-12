71e1f33dca5e5cf57a943f7b32afd888ed29e124
deg84 <t.deg84@gmail.com>
2016-07-12 09:26:18 +0900

    レイヤー切り替え時にLEDが明るすぎたので調整

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -166,6 +166,7 @@ void matrix_scan_user(void) {
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+    ergodox_led_all_set(LED_BRIGHTNESS_LO);
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
         case 1:
