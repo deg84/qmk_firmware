8b52a9a835aa542bb9d2177800d9f18f70a8c76f
deg84 <t.deg84@gmail.com>
2016-07-12 16:00:25 +0900

    親指周りのキー整理

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -18,14 +18,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Esc  | Grv  |      | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L2  |
+ *   |      | Grv  |      | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | LGui | Alt  |       | Alt  | RGui |
+ *                                        |      | Lang2|       | Lang1|      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      | Home |       | PgUp |      |      |
- *                                 |Backsp| Del  |------|       |------| Enter| Space|
- *                                 |ace   |      | End  |       | PgDn |      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 | Space| LGui |------|       |------|Backsp| Enter|
+ *                                 |      |      | Esc  |       | Del  |ace   |      |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -36,19 +36,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,   KC_E,     KC_R,     KC_T,  TG(SYMB),
         KC_LCTL,  KC_A,    KC_S,   KC_D,     KC_F,     KC_G,
         KC_LSFT,  KC_Z,    KC_X,   KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
-        KC_ESC,   KC_GRV,  KC_NO,  KC_LEFT,  KC_RGHT,
-                                               KC_LGUI,KC_LALT,
-                                                       KC_HOME,
-                                         KC_BSPC,KC_DEL,KC_END,
-        // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,  TG(MDIA),
-             KC_RALT, KC_RGUI,
-             KC_PGUP,
-             KC_PGDN, KC_ENT, KC_SPC
+        KC_NO,   KC_GRV,  KC_NO,  KC_LEFT,  KC_RGHT,
+                                                       KC_NO,    KC_LANG2,
+                                                                 KC_NO,
+                                               KC_SPC, KC_LGUI,  KC_ESC,
+
+                                                                          // right hand
+                                                                                KC_RGHT,  KC_6,   KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,
+                                                                               TG(SYMB),  KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,   KC_BSLS,
+                                                                                          KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,   KC_QUOT,
+                                                                           MEH_T(KC_NO),  KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,   KC_RSFT,
+                                                                                                 KC_UP,  KC_DOWN,  KC_LBRC,  KC_RBRC,  TG(MDIA),
+                                                                            KC_LANG1,    KC_NO,
+                                                                               KC_NO,
+                                                                              KC_DEL,  KC_BSPC,  KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
