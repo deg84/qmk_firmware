2aa9f9843686bb01d5668270f31a1cad46a01f6d
deg84 <t.deg84@gmail.com>
2016-07-12 14:19:08 +0900

    macでは使わないInsertキーが残っていたので削除

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Esc  | Grv  |Insert| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L2  |
+ *   | Esc  | Grv  |      | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LGui | Alt  |       | Alt  | RGui |
@@ -32,11 +32,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_LEFT,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  TG(SYMB),
-        KC_LCTL,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
-        KC_ESC,   KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
+        KC_EQL,   KC_1,    KC_2,   KC_3,     KC_4,     KC_5,  KC_LEFT,
+        KC_TAB,   KC_Q,    KC_W,   KC_E,     KC_R,     KC_T,  TG(SYMB),
+        KC_LCTL,  KC_A,    KC_S,   KC_D,     KC_F,     KC_G,
+        KC_LSFT,  KC_Z,    KC_X,   KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
+        KC_ESC,   KC_GRV,  KC_NO,  KC_LEFT,  KC_RGHT,
                                                KC_LGUI,KC_LALT,
                                                        KC_HOME,
                                          KC_BSPC,KC_DEL,KC_END,
