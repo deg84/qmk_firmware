7596d1ed743e780b0c8ffaf3ffbd65e4c8948346
deg84 <t.deg84@gmail.com>
2016-09-07 19:24:41 +0900

    打ち間違いが多すぎてストレスなのでYを左人差し指で押せるように設定

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,   KC_1,   KC_2,   KC_3,    KC_4,     KC_5,  KC_NO,
-        KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,  TG(SYMB),
+        KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,  KC_Y,
         KC_LCTL,  KC_A,   KC_S,   KC_D,    KC_F,     KC_G,
         KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,     KC_B,  ALL_T(KC_NO),
         KC_LALT,  KC_NO,  KC_NO,  KC_ESC,  KC_LGUI,
