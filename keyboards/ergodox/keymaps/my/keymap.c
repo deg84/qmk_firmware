bc4aa59fac73f2dbdd194f2203a2ac4c6ba3be32
deg84 <t.deg84@gmail.com>
2016-11-27 15:59:15 +0900

    []の位置変更

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |   [  |           |  ]   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   Grv  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LAlt | LGui |      |      |  EN  |                                       |JA/GUI| RAlt |   [  |   ]  |      |
+ *   | LAlt | LGui |      |      |  EN  |                                       |JA/GUI| RAlt |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      | ESC  |
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,     KC_2,   KC_3,   KC_4,     KC_5,  KC_NO,
+        KC_EQL,   KC_1,     KC_2,   KC_3,   KC_4,     KC_5,  KC_LBRC,
         KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,     KC_T,  KC_Y,
         KC_LCTL,  KC_A,     KC_S,   KC_D,   KC_F,     KC_G,
         KC_LSFT,  KC_Z,     KC_X,   KC_C,   KC_V,     KC_B,  ALL_T(KC_NO),
@@ -42,11 +42,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     GUI_T(KC_SPC),  KC_BSPC,  KC_NO,
 
                                                                           // right hand
-                                                                                  KC_NO,  KC_6,   KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,
-                                                                                 TG(FN),  KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,   KC_BSLS,
-                                                                                          KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,   KC_QUOT,
-                                                                           MEH_T(KC_NO),  KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,    KC_GRV,
-                                                                                       GUI_T(KC_LANG1),  KC_RALT,  KC_LBRC,  KC_RBRC,     KC_NO,
+                                                                                KC_RBRC,  KC_6,   KC_7,     KC_8,    KC_9,     KC_0,  KC_MINS,
+                                                                                 TG(FN),  KC_Y,   KC_U,     KC_I,    KC_O,     KC_P,  KC_BSLS,
+                                                                                          KC_H,   KC_J,     KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
+                                                                           MEH_T(KC_NO),  KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,   KC_GRV,
+                                                                                       GUI_T(KC_LANG1),  KC_RALT,   KC_NO,    KC_NO,    KC_NO,
                                                                                KC_NO,   KC_ESC,
                                                                                KC_NO,
                                                                                KC_NO,   KC_DELT,  CTL_T(KC_ENT)
