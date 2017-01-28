28846d08870a9ed10e3abc3855ee4cba2e121639
deg84 <t.deg84@gmail.com>
2017-01-28 12:44:04 +0900

    一番上の列をUS配列に合わせて変更する前に戻す
    数字の数が左右で異なる違和感が拭えなかった。もしかしたらまた戻すかもしれない

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ` ~   |  1 ! |  2 @ |  3 # |  4 $ |  5 % |  6 ^ |           |  7 & |  8 * |  9 ( |  0 ) |  - _ |  = + |   BS   |
+ * |  = +   |  1 ! |  2 @ |  3 # |  4 $ |  5 % |      |           |      |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |  L1  |   Y  |   U  |   I  |   O  |   P  |  [ {   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -18,32 +18,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  , < |  . > |  / ? | ] }/SFT|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  fn  | LAlt | LGui |      |EN/GUI|                                       |JA/GUI| RAlt |      |      |  \ | |
+ *   |`~ /fn| LAlt | LGui |      |EN/GUI|                                       |JA/GUI| RAlt |      |      |  \ | |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |  (   |       |   )  | ESC  |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |  [   |       |   ]  |      |      |
- *                                 | Space|      |------|       |------|      | Enter|
- *                                 | /Gui |      |  {   |       |   }  |      | /fn  |
+ *                                 | Space| Back |------|       |------|      | Enter|
+ *                                 | /Gui | Space|  {   |       |   }  |      | /fn  |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,     KC_2,     KC_3,   KC_4,     KC_5,  KC_6,
-        KC_TAB,         KC_Q,     KC_W,     KC_E,   KC_R,     KC_T,  KC_NO,
-        KC_LCTL,        KC_A,     KC_S,     KC_D,   KC_F,     KC_G,
-        KC_LSFT,        KC_Z,     KC_X,     KC_C,   KC_V,     KC_B,  KC_NO,
-        LT(FN, KC_NO),  KC_LALT,  KC_LGUI,  KC_NO,  GUI_T(KC_LANG2),
+        KC_EQL,          KC_1,     KC_2,     KC_3,   KC_4,     KC_5,  KC_NO,
+        KC_TAB,          KC_Q,     KC_W,     KC_E,   KC_R,     KC_T,  KC_NO,
+        KC_LCTL,         KC_A,     KC_S,     KC_D,   KC_F,     KC_G,
+        KC_LSFT,         KC_Z,     KC_X,     KC_C,   KC_V,     KC_B,  KC_NO,
+        LT(FN, KC_GRV),  KC_LALT,  KC_LGUI,  KC_NO,  GUI_T(KC_LANG2),
 
-                                                    KC_NO,  KC_LPRN,
-                                                            KC_LBRC,
-                                    GUI_T(KC_SPC),  KC_NO,  KC_LCBR,
+                                                    KC_NO,    KC_LPRN,
+                                                              KC_LBRC,
+                                    GUI_T(KC_SPC),  KC_BSPC,  KC_LCBR,
 
                                                                           // right hand
-                                                                                   KC_7,  KC_8,   KC_9,     KC_0,  KC_MINS,   KC_EQL,         KC_BSPC,
+                                                                                  KC_NO,  KC_6,   KC_7,     KC_8,     KC_9,     KC_0,         KC_MINS,
                                                                                  TG(FN),  KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,         KC_LBRC,
                                                                                           KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,         KC_QUOT,
                                                                                   KC_NO,  KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  SFT_T(KC_RBRC),
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Functions Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ESC   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   DEL  |
+ * |  ESC   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |Teensy|           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |      | DEL  |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                KC_TRNS,  KC_TRNS,
                                                          KC_TRNS,
-                                     KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                     KC_TRNS,  KC_DELT,  KC_TRNS,
                                                                                 // right hand
-                                                                                  KC_F7,    KC_F8,    KC_F9,   KC_F10,    KC_F11,   KC_F12,   KC_DELT,
+                                                                                  KC_F7,    KC_F8,    KC_F9,   KC_F10,    KC_F11,   KC_F12,   KC_TRNS,
                                                                                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,
                                                                                           KC_LEFT,  KC_DOWN,    KC_UP,  KC_RIGHT,  KC_TRNS,   KC_TRNS,
                                                                                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_UP,   KC_TRNS,
