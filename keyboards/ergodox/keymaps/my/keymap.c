2b1557087613dc9db83e63c90a4c0c709ad99813
deg84 <t.deg84@gmail.com>
2021-08-01 10:12:06 +0900

    左の親指長押しでCommandキーに設定

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
  *                                 | Space|Backsp|------|       |------| Del  | Enter|
- *                                 |      |ace   |      |       |      |      | /Ctrl|
+ *                                 | /Gui |ace   |      |       |      |      | /Ctrl|
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LALT,  KC_LGUI,  KC_NO,  KC_NO,  KC_LGUI,
                                                     KC_NO,    KC_NO,
                                                               KC_NO,
-                                           KC_SPC,  KC_BSPC,  KC_NO,
+                                    GUI_T(KC_SPC),  KC_BSPC,  KC_NO,
 
                                                                           // right hand
-                                                                                  KC_NO,  KC_6,   KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,
+                                                                                KC_NO,  KC_6,   KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,
                                                                                TG(SYMB),  KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,   KC_BSLS,
                                                                                           KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,   KC_QUOT,
                                                                            MEH_T(KC_NO),  KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,    KC_GRV,
