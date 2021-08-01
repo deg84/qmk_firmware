423bb16ed8a778ea5b765edeee15f435c7363021
deg84 <t.deg84@gmail.com>
2021-08-01 10:12:09 +0900

    MacのUS配列をベースに色々変更
    - 一番上の列をUS配列に合わせて変更
    - Pの隣の \ を [ に変更
    US配列的には [ が正しいが [ ] を並べられない問題はあり。
    - \ は使用頻度が低いので右下に移動
    - 右下長押しでシフトキーになるように設定
    - 左下をFunctionレイヤー切り替えキーに設定（US配列でもfnキーになっている）
    - Hyper, Mehは不要なので削除
    - 左人差し指の Y を削除
    - FunctionレイヤーにESC, Deleteキーを追加

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -10,51 +10,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  = +   |  1 ! |  2 @ |  3 # |  4 $ |  5 % |  [ { |           |  ] } |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
+ * |  ` ~   |  1 ! |  2 @ |  3 # |  4 $ |  5 % |  6 ^ |           |  7 & |  8 * |  9 ( |  0 ) |  - _ |  = + |   BS   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |  \ |   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |  L1  |   Y  |   U  |   I  |   O  |   P  |  [ {   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ; : |  ' "   |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  , < |  . > |  / ? |  ` ~   |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  , < |  . > |  / ? | ] }/SFT|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LAlt | LGui |      |      |EN/GUI|                                       |JA/GUI| RAlt |      |      |      |
+ *   |  fn  | LAlt | LGui |      |EN/GUI|                                       |JA/GUI| RAlt |      |      |  \ | |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      | ESC  |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 | Space|Backsp|------|       |------| Del  | Enter|
- *                                 | /Gui |ace   |      |       |      |      | /Ctrl|
+ *                                 | Space|      |------|       |------|      | Enter|
+ *                                 | /Gui |      |      |       |      |      | /fn  |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,     KC_2,   KC_3,   KC_4,     KC_5,  KC_LBRC,
-        KC_TAB,   KC_Q,     KC_W,   KC_E,   KC_R,     KC_T,  KC_Y,
-        KC_LCTL,  KC_A,     KC_S,   KC_D,   KC_F,     KC_G,
-        KC_LSFT,  KC_Z,     KC_X,   KC_C,   KC_V,     KC_B,  ALL_T(KC_NO),
-        KC_LALT,  KC_LGUI,  KC_NO,  KC_NO,  GUI_T(KC_LANG2),
-                                                    KC_NO,    KC_NO,
-                                                              KC_NO,
-                                    GUI_T(KC_SPC),  KC_BSPC,  KC_NO,
+        KC_GRV,         KC_1,     KC_2,     KC_3,   KC_4,     KC_5,  KC_6,
+        KC_TAB,         KC_Q,     KC_W,     KC_E,   KC_R,     KC_T,  KC_NO,
+        KC_LCTL,        KC_A,     KC_S,     KC_D,   KC_F,     KC_G,
+        KC_LSFT,        KC_Z,     KC_X,     KC_C,   KC_V,     KC_B,  KC_NO,
+        LT(FN, KC_NO),  KC_LALT,  KC_LGUI,  KC_NO,  GUI_T(KC_LANG2),
+
+                                                    KC_NO,  KC_NO,
+                                                            KC_NO,
+                                    GUI_T(KC_SPC),  KC_NO,  KC_NO,
 
                                                                           // right hand
-                                                                                KC_RBRC,  KC_6,   KC_7,     KC_8,    KC_9,     KC_0,  KC_MINS,
-                                                                                 TG(FN),  KC_Y,   KC_U,     KC_I,    KC_O,     KC_P,  KC_BSLS,
-                                                                                          KC_H,   KC_J,     KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
-                                                                           MEH_T(KC_NO),  KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,   KC_GRV,
-                                                                                       GUI_T(KC_LANG1),  KC_RALT,   KC_NO,    KC_NO,    KC_NO,
-                                                                               KC_NO,   KC_ESC,
+                                                                                   KC_7,  KC_8,   KC_9,     KC_0,  KC_MINS,   KC_EQL,         KC_BSPC,
+                                                                                 TG(FN),  KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,         KC_LBRC,
+                                                                                          KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,         KC_QUOT,
+                                                                                  KC_NO,  KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  SFT_T(KC_RBRC),
+                                                                                       GUI_T(KC_LANG1),  KC_RALT,    KC_NO,    KC_NO,         KC_BSLS,
+
+                                                                               KC_NO,  KC_ESC,
                                                                                KC_NO,
-                                                                               KC_NO,   KC_DELT,  CTL_T(KC_ENT)
+                                                                               KC_NO,   KC_NO,  LT(FN, KC_ENT)
     ),
 /* Keymap 1: Functions Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |        |
+ * |  ESC   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   DEL  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -75,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // FUNCTIONS
 [FN] = KEYMAP(
        // left hand
-       KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
+        KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -84,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                          KC_TRNS,
                                      KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                                                 // right hand
-                                                                                  KC_F7,    KC_F8,    KC_F9,   KC_F10,    KC_F11,   KC_F12,   KC_TRNS,
+                                                                                  KC_F7,    KC_F8,    KC_F9,   KC_F10,    KC_F11,   KC_F12,   KC_DELT,
                                                                                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,
                                                                                           KC_LEFT,  KC_DOWN,    KC_UP,  KC_RIGHT,  KC_TRNS,   KC_TRNS,
                                                                                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_UP,   KC_TRNS,
