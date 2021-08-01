6aa3cba8f6ab8c38a7af50c78bf9f6f16d768301
deg84 <t.deg84@gmail.com>
2021-08-01 10:12:21 +0900

    人差し指周りのキー設定変更

--- keyboards/ergodash/rev1/keymaps/my/keymap.c
+++ keyboards/ergodash/rev1/keymaps/my/keymap.c
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  `   |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |   6  |   7  |   8  |   9  |   0  |  -   |
+   * |  `   |   1  |   2  |   3  |   4  |   5  |      |                    |      |   6  |   7  |   8  |   9  |   0  |  -   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   (  |                    |   )  |   Y  |   U  |   I  |   O  |   P  |  =   |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |                    |      |   Y  |   U  |   I  |   O  |   P  |  =   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Alt  |   A  |   S  |   D  |   F  |   G  |   {  |                    |   }  |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Alt  |   A  |   S  |   D  |   F  |   G  |   [  |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Game |                    |Adjust|   N  |   M  |   ,  |   .  |   /  |\/Sft |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -41,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_BASE] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                          KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LRBC,                          KC_RRBC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL , \
-    KC_LALT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LCBC,                          KC_RCBC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,                          XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,                          XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL , \
+    KC_LALT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                          KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TGGM,                          ADJUST , KC_N,    KC_M,    KC_COMM, KC_DOT , KC_SLSH, KC_BSST, \
     KC_LCTL, KC_LGUI, KC_LALT, KC_ESC,           KC_MHEN, KC_SCTL, KC_BSMT,        KC_BSPC, KC_ETMT, KC_HENK,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
   ),
