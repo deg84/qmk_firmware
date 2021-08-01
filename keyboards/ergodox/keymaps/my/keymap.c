c5c431f6aa414355845ab0f332fe51291855aeb4
deg84 <t.deg84@gmail.com>
2021-08-01 10:12:03 +0900

    cmdキー、日本語・英語の切り替えキーが押しづらいので、karabinerでcmdキーで切り替え出来るように設定し上で親指周りを再調整

--- keyboards/ergodox/keymaps/my/keymap.c
+++ keyboards/ergodox/keymaps/my/keymap.c
@@ -18,38 +18,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   Grv  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LAlt | LGui |      | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L2  |
+ *   | LAlt |      |      |  ESC | LGui |                                       | RGui | RAlt |   [  |   ]  | ~L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Lang2|      |       |      | Lang1|
+ *                                        | Left |  Up  |       | Down | Right|
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 | Space| LGui |------|       |------|Backsp| Enter|
- *                                 |      |      | Esc  |       | Del  |ace   |      |
+ *                                 |      |      | Home |       | PgUp |      |      |
+ *                                 | Space|Backsp|------|       |------|LShif+| Enter|
+ *                                 |      |ace   | End  |       | PgDn |Enter |      |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,     KC_2,   KC_3,     KC_4,     KC_5,  KC_NO,
-        KC_TAB,   KC_Q,     KC_W,   KC_E,     KC_R,     KC_T,  TG(SYMB),
-        KC_LCTL,  KC_A,     KC_S,   KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,     KC_X,   KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
-        KC_LALT,  KC_LGUI,  KC_NO,  KC_LEFT,  KC_RGHT,
-                                                        KC_LANG2, KC_NO,
-                                                                  KC_NO,
-                                               KC_SPC,  KC_LGUI,  KC_ESC,
+        KC_EQL,   KC_1,   KC_2,   KC_3,    KC_4,     KC_5,  KC_NO,
+        KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,  TG(SYMB),
+        KC_LCTL,  KC_A,   KC_S,   KC_D,    KC_F,     KC_G,
+        KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,     KC_B,  ALL_T(KC_NO),
+        KC_LALT,  KC_NO,  KC_NO,  KC_ESC,  KC_LGUI,
+                                                     KC_LEFT,  KC_UP,
+                                                               KC_HOME,
+                                            KC_SPC,  KC_BSPC,  KC_END,
 
                                                                           // right hand
                                                                                   KC_NO,  KC_6,   KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,
                                                                                TG(SYMB),  KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,   KC_BSLS,
                                                                                           KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,   KC_QUOT,
                                                                            MEH_T(KC_NO),  KC_N,   KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,    KC_GRV,
-                                                                                                 KC_UP,  KC_DOWN,  KC_LBRC,  KC_RBRC,  TG(MDIA),
-                                                                               KC_NO, KC_LANG1,
-                                                                               KC_NO,
-                                                                              KC_DEL,  KC_BSPC,  KC_ENT
+                                                                                               KC_RGUI,  KC_RALT,  KC_LBRC,  KC_RBRC,  TG(MDIA),
+                                                                        KC_DOWN,       KC_RGHT,
+                                                                        KC_PGUP,
+                                                                        KC_PGDN,  LSFT(KC_ENT),  KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
